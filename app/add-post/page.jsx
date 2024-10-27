ca1c1f5a0f434fee7c2966f7e5243d9c6596f933
Nikjo123 <92309098+Nikjo123@users.noreply.github.com>
2024-10-27 21:44:12 +0530

    initial

---
+++ app/add-post/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+import styles from '@/app/page.module.css'
+
+export default function AddPost() {
+    return(
+        <main className={styles.main}>
+            <h1>Add Post</h1>
+        </main>
+    )
+}
